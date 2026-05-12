5381fa2ac782ba35509efe3830168dbb8f5cde0d
Kauan Peçanha <kauanpecanha.dev@gmail.com>
2026-05-12 17:00:07 -0300

    implementing users remaining fields

--- internal/models/User.go
+++ internal/models/User.go
@@ -13,6 +13,11 @@ type User struct {
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	Address   string    `json:"address"`
+	CEP       string    `json:"cep"`
+	Lat       float64   `json:"lat"`
+	Lon       float64   `json:"lon"`
+	Points    int       `gorm:"default:0" json:"points"`
 }
 
 func (u *User) BeforeCreate(tx any) error {

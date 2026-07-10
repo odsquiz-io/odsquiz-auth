2a3521953e9f1b9face88b6907ee550f6fba1bc1
Kauan Peçanha <kauanpecanha.dev@gmail.com>
2026-07-10 16:55:40 -0300

    fix password hashing

--- internal/utils/Hash.go
+++ internal/utils/Hash.go
@@ -1,16 +1,18 @@
 // internal/utils/Hash.go: setup the bcrypt usage to manage encrypted passwords (hash and compare) 
 package utils
 
-import (
-    "golang.org/x/crypto/bcrypt"
-)
+import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
 
 func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+	if password == "" || hash == "" {
+		return false
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
